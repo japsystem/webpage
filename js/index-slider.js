353a32b98cba298369f52c8c401641c298ee2b7b
Cargregor <claconchacerero@gmail.com>
2018-11-07 17:49:06 -0400

    Index Slider finished (possible further changes).

--- js/index-slider.js
+++ js/index-slider.js
@@ -1,4 +1,16 @@
-$(".carousel").carousel({
-	interval: 3500;
-	ride: true;
+$(document).ready(function() {
+	$('.carousel').carousel({
+		interval: 3000,
+		ride: true,
+		pause: false,
+		wrap: false
+	});
+
+	$('.carousel').on('slid.bs.carousel', function(){
+		if($('.carousel .carousel-item:last').hasClass('active')){
+			$('.carousel').carousel('pause');
+		}else{
+			$('.carousel').carousel('cycle');
+		}
+	})
 });
